4c7b708d78b49713382c7e153e13f87afa59a4af
Vivek Sehrawat <vivek.sehrawat@publicissapeint.com>
2023-10-19 16:14:30 +0530

    initial commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Vivek Sehrawat on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
