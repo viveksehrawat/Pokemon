b951c1e5d1da2b198e6d2d5c42d1592c6c3413f8
Vivek Sehrawat <vivek.sehrawat@publicissapeint.com>
2023-10-20 17:24:23 +0530

    added round rect view

--- Pokemon/Modules/PokemonModule/Presentation/Views/Component/DetailComponent/PokemonTypeView.swift
+++ Pokemon/Modules/PokemonModule/Presentation/Views/Component/DetailComponent/PokemonTypeView.swift
@@ -28,13 +28,14 @@ struct PokemonTypes: View {
         Text(type.rawValue.capitalized)
             .font(.system(size: 15))
             .padding(.all, 8)
+            
             .background(Color.pokemon(type: type))
             .overlay(
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(.black, lineWidth: 1)
                 )
+            .cornerRadius(8)
             .frame(height: 20)
-
     }
 }
 
--- Pokemon/Modules/PokemonModule/Presentation/Views/Component/DetailComponent/PokemonView.swift
+++ Pokemon/Modules/PokemonModule/Presentation/Views/Component/DetailComponent/PokemonView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct PokemonView: View {
     var pokemonId: Int
     var dismiss: () -> Void
-
+    
+    let types: [PokemonType] = [.electric, .ground, .fairy, .fire]
+    
     var body: some View {
         ScrollView{
-            VStack{
+            VStack(alignment: .leading){
                 HeaderView(dismiss: {
                     dismiss()
                 }, title: "Pokemon", number: "007")
@@ -23,42 +25,44 @@ struct PokemonView: View {
                     ItemView(title: "Height", subTitle: "5'7")
                     Spacer()
                     ItemView(title: "Weight", subTitle: "90.5 Kg")
-
+                    
                 }
                 Spacer(minLength: 30)
                 HStack{
                     ItemView(title: "Gender(s)", subTitle: "Male, Female")
                     Spacer()
                     ItemView(title: "Egg Groups", subTitle: "Monster, Dragon")
-
+                    
                 }
                 Spacer(minLength: 30)
                 HStack{
                     ItemView(title: "Ablities", subTitle: "Blaze, Solar-Power")
                     Spacer()
                     ItemView(title: "Types", subTitle: "Fire Flying")
-
+                    
                 }
                 Spacer(minLength: 30)
-              
+                
+                VStack(alignment: .leading){
                     Text("Weak Against")
-                        .font(.system(size: 15))
+                        .font(.system(size: 18))
                         .bold()
-                
-                
-                HStack{
-                    PokemonTypes(type: .electric)
-                    PokemonTypes(type: .ground)
-                    PokemonTypes(type: .fairy)
-                    PokemonTypes(type: .normal)
+                    Spacer(minLength: 10)
+                    HStack(alignment: .top){
+                        PokemonTypes(type: types[0])
+                        PokemonTypes(type: types[1])
+                        PokemonTypes(type: types[2])
+                        PokemonTypes(type: types[3])
+                    }
                 }
-                    
+                Spacer(minLength: 30)
+                
                 StatsView()
             }
         }
         .padding(.horizontal, 30)
         .background(Color("backgroundColor"))
-
+        
     }
 }
 
